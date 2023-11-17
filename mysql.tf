bfe05de22409e2d258156fff4df260ba2c46d35b
tungUAS <Tung.Trinh@senacor.com>
2023-11-17 13:42:02 +0100

    add mysql instance

--- mysql.tf
+++ mysql.tf
@@ -1,9 +1,3 @@
-resource "google_sql_database" "database" {
-  name     = "my-database"
-  instance = google_sql_database_instance.instance.name
-}
-
-# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
 resource "google_sql_database_instance" "instance" {
   name             = "my-database-instance"
   region           = var.region
