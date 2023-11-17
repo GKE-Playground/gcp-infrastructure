3d9920e27bc0bfe1979cd4988eafeb2a4d4a744f
tungUAS <Tung.Trinh@senacor.com>
2023-11-17 14:00:25 +0100

    add mysql instance

--- mysql.tf
+++ mysql.tf
@@ -3,14 +3,8 @@ resource "google_sql_database_instance" "sql-instance-psa" {
   region           = var.region
   database_version = "MYSQL_8_0"
 
-  depends_on = [google_service_networking_connection.default]
-
   settings {
     tier = "db-custom-2-7680"
-    ip_configuration { 
-      ipv4_enabled    = "false"
-      private_network = "projects/${var.project_id}/global/networks/${module.vpc-sql-module.network_name}"
-    }
   }
   deletion_protection = false
 }
