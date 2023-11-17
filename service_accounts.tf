5e8976574d37aad2e2e58382a723042ae9859b17
tungUAS <Tung.Trinh@senacor.com>
2023-11-17 11:24:28 +0100

    add roles

--- service_accounts.tf
+++ service_accounts.tf
@@ -8,3 +8,21 @@ resource "google_project_iam_member" "project" {
   role    = "roles/artifactregistry.writer"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
+
+resource "google_project_iam_member" "project" {
+  project = var.project_id
+  role    = "roles/container.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "project" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "project" {
+  project = var.project_id
+  role    = "roles/iam.workloadIdentityUser"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
