18f36e3feab59ad2fc6b53bc2e1b5ff850af9fec
tungUAS <Tung.Trinh@senacor.com>
2023-11-20 11:17:39 +0100

    add postgres database

--- postgres.tf
+++ postgres.tf
@@ -20,24 +20,16 @@ resource "google_sql_database" "postgres_sql_database" {
   instance = google_sql_database_instance.postgres_sql_instance.name
 }
 
-resource "google_sql_table" "example_table" {
-  name     = "example-table"
-  database = google_sql_database.postgres_sql_database.name
-  instance = google_sql_database_instance.postgres_sql_instance.name
+resource "null_resource" "create_table" {
+  depends_on = [google_sql_database.postgres_sql_database]
 
-  column {
-    name = "id"
-    type = "SERIAL"
-    constraints = ["PRIMARY KEY"]
-  }
-
-  column {
-    name = "name"
-    type = "VARCHAR(255)"
-  }
-
-  column {
-    name = "age"
-    type = "INT"
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud sql databases execute \
+      --project=${var.project_id} \
+      --instance=${google_sql_database_instance.postgres_sql_instance.name} \
+      --database=${google_sql_database.postgres_sql_database.name} \
+      --sql='CREATE TABLE IF NOT EXISTS your_table_name (id SERIAL PRIMARY KEY, name VARCHAR(255), age INT);'
+    EOT
   }
 }
