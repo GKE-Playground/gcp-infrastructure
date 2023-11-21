26017e7040ef1dba3f1c6d2c75ca353a34302e27
tungUAS <Tung.Trinh@senacor.com>
2023-11-21 11:32:13 +0100

    add a basic autopilot gke

---
+++ todo-gke.tf
@@ -0,0 +1,10 @@
+resource "google_container_cluster" "primary" {
+  name     = "todo-gke"
+  location = var.region
+ 
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+ 
+# Enabling Autopilot for this cluster
+  enable_autopilot = true
+}
