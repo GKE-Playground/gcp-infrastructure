3bdbdf5e3479c80e2173c69e0c00e8afc6e18287
tungUAS <Tung.Trinh@senacor.com>
2023-11-21 11:41:50 +0100

    add a basic autopilot gke

--- todo-project.tf
+++ todo-project.tf
@@ -11,7 +11,7 @@ resource "google_compute_subnetwork" "todo_subnet" {
   name          = "todo-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  ip_cidr_range = "10.10.10.0/24"
   project       = var.project_id
 }
 
