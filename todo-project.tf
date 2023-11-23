c444e20c5ebe194aad48aeabc85644cf70119c08
tungUAS <Tung.Trinh@senacor.com>
2023-11-23 09:50:35 +0100

    update structure vpc

--- todo-project.tf
+++ todo-project.tf
@@ -1,20 +1,3 @@
-# VPC
-resource "google_compute_network" "todo_vpc" {
-  name                    = "todo-vpc"
-  auto_create_subnetworks = "false"
-  mtu                     = 1460
-  project                 = var.project_id
-}
-
-# Subnet
-resource "google_compute_subnetwork" "todo_subnet" {
-  name          = "todo-subnet"
-  region        = var.region
-  network       = google_compute_network.todo_vpc.name
-  ip_cidr_range = "10.10.10.0/24"
-  project       = var.project_id
-}
-
 resource "google_container_cluster" "todo_gke" {
   name     = "todo-gke"
   location = var.region
