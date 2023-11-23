3282f813a2dd66b93e6348bbe06a061ca81d7bde
tungUAS <Tung.Trinh@senacor.com>
2023-11-23 14:09:40 +0100

    add provider

--- workload-identity-federation.tf
+++ workload-identity-federation.tf
@@ -4,3 +4,25 @@ resource "google_iam_workload_identity_pool" "todo_tfc_identity_pool" {
   description                   = "Production Pool Todo"
   disabled                      = false
 }  
+
+resource "google_iam_workload_identity_pool_provider" "tfc_pool_provider" {
+  workload_identity_pool_id           	= google_iam_workload_identity_pool.tfc_identity_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id  	= "terraform-provider-todo"
+  display_name                        		= "terraform-provider-todo"
+  description                       		= "Terraform Cloud OIDC Provider"
+  disabled                        			= false
+
+  attribute_mapping                   = {
+    "attribute.tfc_organization_id"   	= "assertion.terraform_organization_id"
+    "attribute.tfc_project_id"        		= "assertion.terraform_project_id"
+    "attribute.tfc_project_name"      	= "assertion.terraform_project_name"
+    "google.subject"                  		= "assertion.terraform_workspace_id"
+    "attribute.tfc_workspace_name"    	= "assertion.terraform_workspace_name"
+    "attribute.tfc_workspace_env"     	= "assertion.terraform_workspace_name.split('-')[assertion.terraform_workspace_name.split('-').size() -1]"
+  }
+
+  oidc {
+       issuer_uri        = "https://app.terraform.io"
+  }
+
+}
