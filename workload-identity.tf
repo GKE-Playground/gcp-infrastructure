24538906d7b97cbee03f7beeb0e0e8b70f3b7f71
tungUAS <Tung.Trinh@senacor.com>
2023-11-24 10:45:39 +0100

    add logWritter role to gke sa

--- workload-identity.tf
+++ workload-identity.tf
@@ -3,5 +3,5 @@ module "my-app-workload-identity" {
   name       = "todo-sa"
   namespace  = kubernetes_namespace.todo_namespace.metadata[0].name
   project_id = var.project_id
-  roles      = ["roles/storage.admin", "roles/compute.admin", "roles/cloudsql.client"]
+  roles      = ["roles/storage.admin", "roles/compute.admin", "roles/cloudsql.client", "roles/logging.logWriter"]
 }
